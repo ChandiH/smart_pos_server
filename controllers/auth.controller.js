c4e4786bc6a43f75aa2466471eb6a92d8769383c
Somesh Chandimal <someshchandih@gmail.com>
2023-09-14 00:10:41 +0530

    fix login issue

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,21 +1,31 @@
 const Auth = require("../models/auth.model");
+const Employee = require("../models/employee.model");
 const jwt = require("jsonwebtoken");
 
 module.exports = {
   async login(req, res, next) {
     const { username, password } = req.body;
-    const data = await Auth.login(username, password)
-      .then((data) => data)
-      .catch((err) => res.status(400).json({ error: err }));
-    if (!data.rows[0]) {
-      return res.status(400).json({ error: "user not find" });
-    }
-    const user = { ...data.rows[0] };
-    delete user["password"];
-    const token = await jwt.sign(user, process.env.SECRET_KEY, {
-      algorithm: "HS256",
-    });
 
-    return res.status(200).json({ token: token });
+    // check whether username and password are correct
+    Auth.login(username, password)
+      .then((data) => {
+        // then get employee_id
+        const employee_id = data.rows[0]?.employee_id;
+        if (!employee_id) {
+          return res.status(400).json({ error: "Invalid username/password" });
+        }
+        // then get employee details
+        Employee.getEmployee(employee_id)
+          .then((data) => {
+            delete data.rows[0]["password"];
+            // then generate token
+            const token = jwt.sign(data.rows[0], process.env.SECRET_KEY, {
+              algorithm: "HS256",
+            });
+            return res.status(200).json({ token: token });
+          })
+          .catch((err) => res.status(400).json({ error: err }));
+      })
+      .catch((err) => res.status(400).json({ error: err }));
   },
 };
--- models/auth.model.js
+++ models/auth.model.js
@@ -4,7 +4,7 @@ const pool = require("../config/config");
 
 const login = (username, password) => {
   return pool.query(
-    "select * from employee WHERE username = $1 AND password = crypt($2, password)",
+    "select employee.employee_id from employee WHERE username = $1 AND password = crypt($2, password)",
     [username, password]
   );
 };
