5dc28f57af658032b98dac89673bcad7c02f587a
Somesh Chandimal <someshchandih@gmail.com>
2023-10-03 23:20:24 +0530

    detach env variable

--- config/config.js
+++ config/config.js
@@ -1,11 +1,12 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "postgres",
-  database: "smart_pos_db",
-  host: "localhost",
-  port: 8088,
-  password: "password",
+  user: process.env.DATABASE_USER,
+  database: process.env.DATABASE_NAME,
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  password: process.env.DATABASE_PASSWORD,
+  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pool;
